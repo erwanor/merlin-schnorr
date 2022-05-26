e66e82b2cda348eb9569b15a654cfc2a9c2ded31
erwanor <erwan.ounn.84@gmail.com>
2022-05-26 17:26:03 +0200

    feat: add the make-believe FS transform

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use curve25519_dalek_ng::{self, constants, ristretto, scalar::Scalar};
+use curve25519_dalek_ng::{self, constants, ristretto::RistrettoPoint, scalar::Scalar};
+use merlin::Transcript;
 use rand_core::OsRng;
-
 fn main() {
+    let transcript = Transcript::new(b"schnorrid");
+
     println!("Hello, world!");
     let mut rng = OsRng;
 
@@ -37,4 +39,28 @@ fn main() {
     let target = g * z;
     let ver = u + h * challenge;
     println!("checks out? {}", target == ver);
+
+    // NI version
+    let mut transcript = Transcript::new(b"schnorrid");
+
+    transcript.append_message(b"generator", &g.compress().to_bytes());
+    let secret = Scalar::random(&mut rng);
+    let h = g * secret;
+
+    let r = Scalar::random(&mut rng);
+    let u = g * r;
+
+    transcript.append_message(b"field order", &constants::BASEPOINT_ORDER.to_bytes());
+    transcript.append_message(b"committed secret", &h.compress().to_bytes());
+    transcript.append_message(b"randomness commitment", &u.compress().to_bytes());
+
+    let mut transcript_rng = transcript.build_rng().finalize(&mut rng);
+    let challenge = Scalar::random(&mut transcript_rng);
+    let semi_proof = secret * challenge;
+    let proof = r + semi_proof;
+
+    let proof_eval = proof * g;
+    let eval_commit_semi = challenge * h;
+    let eval_commit = eval_commit_semi + u;
+    println!("checks out? {}", proof_eval == eval_commit);
 }
